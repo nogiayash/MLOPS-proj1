780d4b857a5a5b02fa05815b8af8c635d76fba20
yashwant nogia <yash280pali@gmail.com>
2026-04-09 21:32:52 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yashwant Nogia",
+    author_email="yash280pali@gmail.com",
+    packages=find_packages()
+)
